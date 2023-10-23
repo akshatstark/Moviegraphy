532ce132f8566f5ffa6f3e9a941b35e4fe8b487c
Akshat Singh <captainakkinemo@gmail.com>
2023-10-23 13:30:25 +0530

    made main page-background widget

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,14 +1,53 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainPage extends ConsumerWidget{
+class MainPage extends ConsumerWidget {
+  late double _devHeight;
+  late double _devWidth;
 
   @override
   Widget build(BuildContext context, WidgetRef watch) {
+    _devHeight = MediaQuery.of(context).size.height;
+    _devWidth = MediaQuery.of(context).size.width;
     return buildUI();
   }
-  Widget buildUI(){
-    return Scaffold();
-}
+
+  Widget buildUI() {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        height: _devHeight,
+        width: _devWidth,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _backgroundWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+        height: _devHeight,
+        width: _devWidth,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://www.themoviedb.org/t/p/w220_and_h330_face/NNxYkU70HPurnNCSiCjYAmacwm.jpg'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(20)),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+          child: Container(
+            decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+          ),
+        )
+    );
+  }
 }
